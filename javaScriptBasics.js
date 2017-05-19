cd897c246fc8391aadb0e5aa4d04b33e2e899a82
Eric de Anda <edeandae7@gmail.com>
2017-05-18 18:45:40 -0700

    basic practice with javaScript

---
+++ javaScriptBasics.js
@@ -0,0 +1,48 @@
+"hello" //to write hello in the command line
+"cat".length //finding the lenght of the string cat
+
+confirm("I am awesome"); //asking the user to ok or cancel whether or not I am awesome
+
+prompt("what is your name?"); //asking an answer from the user, a pop box to write something will appear
+
+"I'm coding like a champ".length >10 //to make a boolean comparizon like whener a string length is greater than a number do like this--- 
+
+//console print out
+console.log(2*5) //this prints 10
+console.log("Hello") //this prints Hello
+
+/*comparizon operators
+
+> Greater than
+< Less than
+<= Less than or equal to
+>= Greater than or equal to
+=== Equal to
+!== Not equal to
+*/
+console.log(8*2 === 16); //this prints true
+
+//if statements
+if (99>5 ) {
+    console.log("yep 99 is greater than 5" );//this prints yep 99 is greater than 5
+}
+
+//else statements
+
+if ( "Eric".length>7) 
+{
+    console.log("Let's go down the first road!");
+}
+else 
+{
+    console.log("eric is a short name");
+    // What should we do if the condition is false? Fill in here:
+    
+}
+
+//modulus
+console.log(14%3); //prints 2 divides 14 by 3
+//if( 14%2===0 ) //in an if statement
+
+//substrings
+//From 4th to 6th letter of "laptop" "laptop".substring(3,6);
